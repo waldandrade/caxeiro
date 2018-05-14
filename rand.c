d070e28389f0823d399947d89599efcd9b8b7f6f
georgyljf <georgyljf@hotmail.com>
2018-05-13 23:46:46 -0300

    sem comentario

--- rand.c
+++ rand.c
@@ -1,15 +1,12 @@
-#include <iostream>
 #include <stdio.h>  
 #include <stdlib.h>
 #include <time.h>
-
-using namespace std;
 
 //mudar pra variavel "tam" pra receber a quantia de cidades (para assim fazer a corrente de "1" a N e de volta a "1"
 const int NUM=100;
 int main()
 {
-	cout << "\n\nPrograma para gerar numeros aleatorios"; //pode ser apagado...
+	printf("\n\nPrograma para gerar numeros aleatorios"); //pode ser apagado...
 
 	//declaração das variáveis
 	int vetor[NUM], i=0, j=0, cont_linha=0; //usado pra fazer a lista de linhas randomicas
@@ -34,16 +31,16 @@ int main()
 		}
 	}
 
-	cout << "\n\n";
+	printf("\n\n");
 
 	for(i=0; i<NUM; i++)
 	{
-		cout << vetor[i] << " ";
+		printf("%i\t", vetor[i]);
 		cont_linha++;
 
 		if(cont_linha == 10)
 		{
-			cout << "\n";
+			printf("\n");
 			cont_linha=0;
 		}
 	}
